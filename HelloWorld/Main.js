0ab499accce9019c0db5041ba730e200e46b374f
y-ogata <steelwind666@gmail.com>
2018-04-23 09:43:14 +0900

    Initial commit new file:   HelloWorld/Main.js new file:   HelloWorld/index.html

---
+++ HelloWorld/Main.js
@@ -0,0 +1,11 @@
+class HelloWorld {
+    constructor() {
+        console.log("constructor of HelloWorld Class");
+    }
+
+    hello() {
+        console.log("hello world in console log");
+    }
+}
+const helloWorld = new HelloWorld();
+helloWorld.hello();
