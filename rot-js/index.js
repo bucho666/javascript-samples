55842c1b9ef4d4313e7e2413e0ef79c5990574f9
y-ogata <steelwind666@gmail.com>
2018-04-25 16:21:48 +0900

    rot-jsサンプル、簡単なwalkデモまで実装 modified:   rot-js/index.css modified:   rot-js/index.html modified:   rot-js/index.js

--- rot-js/index.js
+++ rot-js/index.js
@@ -1,14 +1,3 @@
-// documentのキーダウンイベント
-document.onkeydown = (e) => {
-    if (e.repeat) return false;
-    addMessage(`down key is "${e.key}"[${e.keyCode}]`);
-};
-
-// documentのキーアップ
-document.onkeyup = (e) => {
-    addMessage(`up key is "${e.key}"[${e.keyCode}]`);
-}
-
 // メッセージ追加
 function addMessage(message) {
     // p要素作成
@@ -23,12 +12,222 @@ function addMessage(message) {
     messages.scrollTop = messages.scrollHeight;
 }
 
+class Entity {
+  constructor(symbol, color='silver') {
+    this._symbol = symbol;
+    this._color = color;
+  }
+
+  render(x, y, display) {
+    display.draw(x, y, this._symbol, this._color);
+  }
+}
+
+class Terrain extends Entity {
+  constructor(symbol, color='silver', walkable=false) {
+    super(symbol, color);
+    this._walkable = walkable;
+  }
+
+  get isWalkable() { return this._walkable; }
+}
+
+const TerrainTable = {
+  wall: new Terrain(' '),
+  floor: new Terrain('.', 'silver', true),
+  door: new Terrain('+', 'olive', true),
+}
+
+class Random {
+  static number(min, max) {
+    const diff = (max - min) + 1;
+    return Math.floor(Math.random() * diff) + min;
+  }
+}
+
+Array.prototype.randomChoice = function() {
+  return this[Random.number(0, this.length)];
+}
+
+class Map {
+  constructor(width, height) {
+    this._map = new Array(height)
+    for (let y = 0; y < height; y++) {
+      this._map[y] = new Array(width);
+    }
+  }
+
+  render(display) {
+    this.forEach((x, y, terrain)=>{
+      terrain.render(x, y, display);
+    });
+  }
+
+  isWalkable(coordinate) {
+    return this._map[coordinate.y][coordinate.x].isWalkable;
+  }
+
+  openSpaceAtRandom() {
+    const openSpaces = [];
+    this.forEach((x, y, terrain)=>{
+      if (terrain.isWalkable) {
+        openSpaces.push(new Coordinate(x, y));
+      }
+    });
+    return openSpaces.randomChoice();
+  }
+
+  update(x, y, terrain) {
+    this._map[y][x] = terrain;
+  }
+
+  forEach(f) {
+    for (let y = 0; y < this._map.length; y++) {
+      for (let x = 0; x < this._map[y].length; x++) {
+        f(x, y, this._map[y][x]);
+      }
+    }
+  }
+}
+
+class Coordinate {
+  constructor(x=0, y=0) {
+    this._x = x;
+    this._y = y;
+  }
+
+  get x() { return this._x; }
+  get y() { return this._y; }
+
+  plus(otherCoordinate) {
+    return new Coordinate(this.x + otherCoordinate.x, this.y + otherCoordinate.y);
+  }
+}
+
+const Direction = {
+  N : new Coordinate(0 , -1), E : new Coordinate(1 ,  0),
+  S : new Coordinate(0 ,  1), W : new Coordinate(-1,  0),
+  NE: new Coordinate(1 , -1), SE: new Coordinate(1 ,  1),
+  SW: new Coordinate(-1,  1), NW: new Coordinate(-1, -1),
+};
+
+class Player extends Entity {
+  constructor(coordinate = new Coordinate()) {
+    super('@');
+    this.coordinate = coordinate; 
+  }
+
+  render(display) {
+    super.render(this.coordinate.x, this.coordinate.y, display);
+  }
+}
+
+DirectionOfKey = {
+  'k': Direction.N, 'l': Direction.E, 'j': Direction.S, 'h': Direction.W,
+  'u': Direction.NE, 'n': Direction.SE, 'b': Direction.SW, 'y': Direction.NW
+};
+
+class Controller {
+  constructor() {
+    this._key_state = {};
+  }
+
+  keyDown(key) {
+    this._key_state[key] = true;
+  }
+
+  keyUp(key) {
+    if (key in this._key_state) {
+      this._key_state[key] = false
+    }
+  }
+
+  pressed(key) {
+    return key in this._key_state && this._key_state[key];
+  }
+}
+
+class FPS {
+  constructor(fps, f) {
+    this._f = f;
+    this._startTime = 0;
+    this._interval = 1000/fps;
+  }
+
+  start() {
+      window.requestAnimationFrame((timeStamp)=>{this.tick(timeStamp)});
+  }
+
+  tick(timeStamp) {
+    const progress = timeStamp - this._startTime;
+    if (progress > this._interval) {
+      this._f();
+      this._startTime = timeStamp;
+    }
+    window.requestAnimationFrame((timeStamp)=>{this.tick(timeStamp)});
+  }
+}
+
 class Game {
   constructor(screenID) {
+    this.setupDisplay(screenID);
+    this.generateMap();
+    this._player = new Player(this._map.openSpaceAtRandom());
+    this._controller = new Controller();
+    document.onkeydown = (e) => { this._controller.keyDown(e.key) };
+    document.onkeyup = (e) => { this._controller.keyUp(e.key) };
+    const fps = new FPS(30, ()=>{this.update();});
+    fps.start();
+  }
+
+  update() {
+    for (let key in DirectionOfKey) {
+      if (this._controller.pressed(key)) {
+        this.movePlayer(DirectionOfKey[key]);
+      }
+    }
+    this.render();
+  }
+
+  movePlayer(direction) {
+      const newCoordinate = this._player.coordinate.plus(direction);
+      if (this._map.isWalkable(newCoordinate) == false) { return; }
+      this._player.coordinate = newCoordinate;
+  }
+
+  render() {
+    this._map.render(this._display);
+    this._player.render(this._display);
+  }
+
+  generateMap() {
+    const displayOption = this._display.getOptions();
+    const [MAP_WIDTH, MAP_HEIGHT] = [displayOption.width, displayOption.height];
+    this._map = new Map(MAP_WIDTH, MAP_HEIGHT);
+    const digger = new ROT.Map.Digger(MAP_WIDTH, MAP_HEIGHT);
+    digger.create((x, y, isWall)=>{
+      this._map.update(x, y, isWall ? TerrainTable.wall : TerrainTable.floor);
+    });
+    for (const room of digger.getRooms()) {
+      room.getDoors((x, y)=>{
+        this._map.update(x, y, TerrainTable.door);
+      });
+    }
+  }
+
+  setupDisplay(screenID) {
     this._display = new ROT.Display();
+    // display options
+    // width -- horizontal size, in characters
+    // height -- vertical size, in characters
+    // fontSize -- in pixels, default 15
+    // fontFamily -- string, default "monospace"
+    // fg -- default foreground color; valid CSS color string
+    // bg -- default background color; valid CSS color string
+    // spacing -- spacing adjustment coefficient; 1 = normal, <1 tighter, >1 looser
+    // layout -- what layouting algorithm shall be used; "rect" or "hex"
+    this._display.setOptions({ width: 80, height: 24, bg: "white", });
     document.getElementById(screenID).appendChild(this._display.getContainer());
-    this._display.drawText(0, 0, '%c{#0a0}hello rot js!%c{}');
-    this._display.draw(3, 4, '@', 'olive');
   }
 }
 
